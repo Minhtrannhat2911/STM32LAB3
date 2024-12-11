90242532c9f8a9463d5b5722729b9fd6ad54e602
DESKTOP-6NRT369\Dell <minh.trannhat2911@hcmut.edu.vn>
2024-12-12 03:22:09 +0700

    fix lab4

--- LAB3.0/Core/Inc/fsm_manual.h
+++ LAB3.0/Core/Inc/fsm_manual.h
@@ -11,11 +11,17 @@
 #include "main.h"
 #include "global.h"
 
-
+// Các biến toàn cục
 extern int mode;
 extern int redValue;
 extern int yellowValue;
 extern int greenValue;
 
+// Khai báo hàm FSM manual
 void fsm_manual_run();
+
+// Khai báo các hàm hỗ trợ
+void updateBufferAndClear(int newMode, int value);
+void handleButtonPressed(int nextStatus, int newMode, void (*updateFunc)(void), int value);
+
 #endif /* INC_FSM_MANUAL_H_ */
--- LAB3.0/Core/Src/fsm_manual.c
+++ LAB3.0/Core/Src/fsm_manual.c
@@ -13,127 +13,87 @@ int redValue = 0;
 int yellowValue = 0;
 int greenValue = 0;
 
-void fsm_manual_run(){
-	switch(status) {
-			case MAN: {
-				BufferHorizontal(mode);
-				BufferVertical(0);
-				if (timer_flag[3] == 1) {
-					setTimer(3, 500); // Timer 4
-					blinkAllLed(); // Blink 2Hz
-				}
-				if (isButtonPressed(0)) {
-					status = MODE2;
-					mode = 2;
-					// Save original values
-					redValue = redTime;
-					yellowValue = yellowTime;
-					greenValue = greenTime;
-					BufferHorizontal(mode);
-					BufferVertical(redTime);
-					clearTrafficLight();
-				}
-				break;
-			}
-			case MODE2: {
-				if (timer_flag[3] == 1) {
-					setTimer(3, 500); // Timer 4
-					blinkRedLed(); // Blink 2Hz
-				}
-				if (isButtonPressed(0)) { // BTN1 pressed, update status and discard value
-					status = MODE3;
-					mode = 3;
-					// Adjust Time to the original, not save the updated value
-					setRedTime(redValue);
-					setYellowTime(yellowValue);
-					setGreenTime(greenValue);
-					BufferHorizontal(mode);
-					BufferVertical(yellowTime);
-					clearTrafficLight();
-					break;
-				}
-				if (isButtonPressed(1)) { // BTN2 pressed, update value
-					timeredupdate();
-					BufferVertical(redTime);
-					break;
-				}
-				if (isButtonPressed(2)) { // BTN3 pressed, save the value -> update Time
-					status = MAN;
-					setTimer(2, 10000); // Timer 3
-					clearTrafficLight();
-					mode = 0;
-					break;
-				}
-				break;
-			}
-			case MODE3: {
-				if (timer_flag[3] == 1) {
-					setTimer(3, 500); // Timer 4
-					blinkYellowLed(); // Blink 2Hz
-				}
-				if (isButtonPressed(0)) { // BTN1 pressed, update status and discard value
-					status = MODE4;
-					mode = 4;
-					// Adjust Time to the original, not save the updated value
-					setRedTime(redValue);
-					setYellowTime(yellowValue);
-					setGreenTime(greenValue);
-					BufferHorizontal(mode);
-					BufferVertical(greenTime);
-					clearTrafficLight();
-					break;
-				}
-				if (isButtonPressed(1)) { // BTN2 pressed, update value
-					timeyellowupdate();
-					BufferVertical(yellowTime);
-					break;
-				}
-				if (isButtonPressed(2)) { // BTN3 pressed, save the value -> update Time
-					status = MAN;
-					setTimer(2, 10000); // Timer 3
-					clearTrafficLight();
-					mode = 0;
-					break;
-				}
-				break;
-			}
-			case MODE4: {
-				if (timer_flag[3] == 1) {
-					setTimer(3, 500); // Timer 4
-					blinkGreenLed(); // Blink 2Hz
-				}
-				if (isButtonPressed(0)) { // BTN1 pressed, update status and discard value
-					status = MAN;
-					setTimer(2, 10000); // Timer 3
-					clearTrafficLight();
-					mode = 0;
-					// Adjust Time to the original, not save the updated value
-					setRedTime(redValue);
-					setYellowTime(yellowValue);
-					setGreenTime(greenValue);
-					BufferHorizontal(mode);
-					BufferVertical(0);
-					break;
-				}
-				if (isButtonPressed(1)) { // BTN2 pressed, update value
-					timegreenupdate();
-					BufferVertical(greenTime);
-					break;
-				}
-				if (isButtonPressed(2)) { // BTN3 pressed, save the value -> update Time
-					status = MAN;
-					setTimer(2, 10000); // Timer 3
-					clearTrafficLight();
-					mode = 0;
-					break;
-				}
-				break;
-			}
-			default: {
-				break;
-			}
-		}
+/*
+ * fsm_manual.c
+ *
+ *  Created on: Oct 30, 2024
+ *      Author: Dell
+ */
 
+#include "fsm_manual.h"
+#include "light.h"
+
+int mode = 0;
+int redValue = 0;
+int yellowValue = 0;
+int greenValue = 0;
+
+
+void updateBufferAndClear(int newMode, int value) {
+    mode = newMode;
+    BufferHorizontal(mode);
+    BufferVertical(value);
+    clearTrafficLight();
 }
+
+void handleButtonPressed(int nextStatus, int newMode, void (*updateFunc)(void), int value) {
+    if (isButtonPressed(0)) {
+        status = nextStatus;
+        updateBufferAndClear(newMode, value);
+    } else if (isButtonPressed(1) && updateFunc) {
+        updateFunc();
+        BufferVertical(value);
+    } else if (isButtonPressed(2)) {
+        status = MAN;
+        setTimer(2, 10000); // Timer 3
+        clearTrafficLight();
+        mode = 0;
+    }
+}
+
+void fsm_manual_run() {
+    if (timer_flag[3] == 1) {
+        setTimer(3, 500); // Timer 4
+        switch (status) {
+            case MAN: blinkAllLed(); break;
+            case MODE2: blinkRedLed(); break;
+            case MODE3: blinkYellowLed(); break;
+            case MODE4: blinkGreenLed(); break;
+        }
+    }
+
+    switch (status) {
+        case MAN: {
+            BufferHorizontal(mode);
+            BufferVertical(0);
+            if (isButtonPressed(0)) {
+                status = MODE2;
+                redValue = redTime;
+                yellowValue = yellowTime;
+                greenValue = greenTime;
+                updateBufferAndClear(2, redTime);
+            }
+            break;
+        }
+        case MODE2: {
+            handleButtonPressed(MODE3, 3, timeredupdate, redTime);
+            break;
+        }
+        case MODE3: {
+            handleButtonPressed(MODE4, 4, timeyellowupdate, yellowTime);
+            break;
+        }
+        case MODE4: {
+            handleButtonPressed(MAN, 0, timegreenupdate, greenTime);
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+}
+
+
+
 
 
